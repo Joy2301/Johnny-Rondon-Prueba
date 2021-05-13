36fff96df560d8f4234429d2114637a9c3c8efb9
Johnny <el_jhonny23@hotmail.com>
2021-05-13 03:26:14 -0400

    Toques Finales

--- JohnnyR_Prueba/obj/Debug/net5.0/RazorDeclaration/Pages/Personas/Lista.razor.g.cs
+++ JohnnyR_Prueba/obj/Debug/net5.0/RazorDeclaration/Pages/Personas/Lista.razor.g.cs
@@ -13,84 +13,84 @@ namespace JohnnyR_Prueba.Pages.Personas
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\mdela\Desktop\Proyectos\C#\JohnnyR_Prueba\_Imports.razor"
+#line 1 "C:\Users\mdela\Desktop\Proyectos\C#\Johnny-Rondon-Prueba\JohnnyR_Prueba\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\mdela\Desktop\Proyectos\C#\JohnnyR_Prueba\_Imports.razor"
+#line 2 "C:\Users\mdela\Desktop\Proyectos\C#\Johnny-Rondon-Prueba\JohnnyR_Prueba\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\mdela\Desktop\Proyectos\C#\JohnnyR_Prueba\_Imports.razor"
+#line 3 "C:\Users\mdela\Desktop\Proyectos\C#\Johnny-Rondon-Prueba\JohnnyR_Prueba\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\mdela\Desktop\Proyectos\C#\JohnnyR_Prueba\_Imports.razor"
+#line 4 "C:\Users\mdela\Desktop\Proyectos\C#\Johnny-Rondon-Prueba\JohnnyR_Prueba\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\mdela\Desktop\Proyectos\C#\JohnnyR_Prueba\_Imports.razor"
+#line 5 "C:\Users\mdela\Desktop\Proyectos\C#\Johnny-Rondon-Prueba\JohnnyR_Prueba\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\mdela\Desktop\Proyectos\C#\JohnnyR_Prueba\_Imports.razor"
+#line 6 "C:\Users\mdela\Desktop\Proyectos\C#\Johnny-Rondon-Prueba\JohnnyR_Prueba\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\mdela\Desktop\Proyectos\C#\JohnnyR_Prueba\_Imports.razor"
+#line 7 "C:\Users\mdela\Desktop\Proyectos\C#\Johnny-Rondon-Prueba\JohnnyR_Prueba\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\mdela\Desktop\Proyectos\C#\JohnnyR_Prueba\_Imports.razor"
+#line 8 "C:\Users\mdela\Desktop\Proyectos\C#\Johnny-Rondon-Prueba\JohnnyR_Prueba\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\mdela\Desktop\Proyectos\C#\JohnnyR_Prueba\_Imports.razor"
+#line 9 "C:\Users\mdela\Desktop\Proyectos\C#\Johnny-Rondon-Prueba\JohnnyR_Prueba\_Imports.razor"
 using JohnnyR_Prueba;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\mdela\Desktop\Proyectos\C#\JohnnyR_Prueba\_Imports.razor"
+#line 10 "C:\Users\mdela\Desktop\Proyectos\C#\Johnny-Rondon-Prueba\JohnnyR_Prueba\_Imports.razor"
 using JohnnyR_Prueba.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 11 "C:\Users\mdela\Desktop\Proyectos\C#\JohnnyR_Prueba\_Imports.razor"
+#line 11 "C:\Users\mdela\Desktop\Proyectos\C#\Johnny-Rondon-Prueba\JohnnyR_Prueba\_Imports.razor"
 using JohnnyR_Prueba.Data.Personas;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 12 "C:\Users\mdela\Desktop\Proyectos\C#\JohnnyR_Prueba\_Imports.razor"
+#line 12 "C:\Users\mdela\Desktop\Proyectos\C#\Johnny-Rondon-Prueba\JohnnyR_Prueba\_Imports.razor"
 using JohnnyR_Prueba.Models;
 
 #line default
@@ -105,7 +105,7 @@ using JohnnyR_Prueba.Models;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 57 "C:\Users\mdela\Desktop\Proyectos\C#\JohnnyR_Prueba\Pages\Personas\Lista.razor"
+#line 57 "C:\Users\mdela\Desktop\Proyectos\C#\Johnny-Rondon-Prueba\JohnnyR_Prueba\Pages\Personas\Lista.razor"
       
 
     private IEnumerable<Persona> persona { get; set; }
--- Tarea9/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Tarea9.MvcApplication" Language="C#" %>
